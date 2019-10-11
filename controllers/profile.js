093cce9073cfdd1eb65c53f9e08d4155056aed8a
john nguyen <trung.nguyenthanh.2702@gmail.com>
2019-10-11 14:07:25 +0700

    Complated Profile

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,61 +1,158 @@
 const fs = require('fs');
 const dbUser = require('../models/user.model');
 
+exports.getAllProfile = function (req, res) {
+    dbUser.find({}, function (err, doc) {
+        dbUser.findOne({ username: req.user.username }, function (err, admin) {
+            res.render('profile/allprofile', {
+                layout: 'layout',
+                title: 'All Profile',
+                users: doc,
+                admin: admin
+            })
+        })
+    })
+}
+
 exports.getProfile = function (req, res) {
     let username = req.params.username;
     dbUser.findOne({ username: username }, function (err, doc) {
-        var createat = doc.createAt.toLocaleDateString();
-        res.render('profile', {
-            layout: 'layout',
-            title: 'profile',
-            admin: req.user,
-            user: doc,
-            date: createat
+        dbUser.findOne({ username: req.user.username }, function (err, admin) {
+            var createat = doc.createAt.toLocaleDateString();
+            res.render('profile/profile', {
+                layout: 'layout',
+                title: 'Profile',
+                admin: admin,
+                user: doc,
+                date: createat
+            });
+        })
+    })
+}
+
+exports.postProfile = function (req, res) {
+    let username = req.params.username;
+    let rb = req.body,
+        email = rb.email,
+        firstname = rb.firstname,
+        lastname = rb.lastname,
+        phone = rb.phone,
+        country = rb.country,
+        active = rb.active,
+        avatar = req.file,
+        description = rb.description;
+    dbUser.findOne({ username: username }, function (err, doc) {
+        if (email != '') {
+            doc.email = email;
+        }
+        if (firstname != '') {
+            doc.firstname = firstname;
+        }
+        if (lastname != '') {
+            doc.lastname = lastname;
+        }
+        if (phone != '') {
+            doc.phone = phone
+        }
+        if (country != '') {
+            doc.country = country;
+        }
+        doc.active = active;
+        if (avatar != undefined) {
+            avatar = req.file.filename;
+            if (doc.avatar == 'default-avatar.png') {
+                doc.avatar = avatar;
+            } else {
+                fs.unlink('public/img/user/' + doc.avatar, function () { });
+                doc.avatar = avatar;
+            }
+        }
+        doc.description = description;
+        doc.save((err) => {
+            if (!err) {
+                res.redirect('/profile');
+            }
         });
     })
 }
 
-exports.postProfile = function(req, res){
+exports.seeOneProfile = function (req, res) {
     let username = req.params.username;
+    dbUser.findOne({ username: username }, function (err, doc) {
+        dbUser.findOne({ username: req.user.username }, function (err, admin) {
+            var createat = doc.createAt.toLocaleDateString();
+            res.render('profile/seeoneprofile', {
+                layout: 'layout',
+                title: 'See Profile',
+                admin: admin,
+                user: doc,
+                date: createat
+            });
+        })
+    })
+}
+
+exports.deleteProfile = function (req, res) {
+    var id = req.params.id;
+    dbUser.findByIdAndRemove(id).exec(function (err, doc) {
+        if (doc.avatar != 'default-avatar.png') {
+            fs.unlink('public/img/user/' + doc.avatar, function () { })
+        }
+        res.redirect('/profile');
+    })
+}
+
+exports.addProfile = function (req, res) {
+    res.render('profile/addprofile', {
+        layout: 'layout',
+        title: 'Addd Profile',
+        admin: req.user,
+        error: req.flash('error')
+    });
+}
+
+exports.addNewProfile = function (req, res) {
     let rb = req.body,
-    email = rb.email,
-    firstname = rb.firstname,
-    lastname = rb.lastname,
-    phone = rb.phone,
-    country = rb.country,
-    active = rb.active,
-    avatar = req.file,
-    description = rb.description;
-    dbUser.findOne({username: username}, function(err, doc){
-        if(email != ''){
-            doc.email = email;
+        username = rb.username,
+        password = rb.password,
+        email = rb.email,
+        firstname = rb.firstname,
+        lastname = rb.lastname,
+        phone = rb.phone,
+        country = rb.country,
+        active = rb.active,
+        avatar = req.file,
+        description = rb.description;
+    dbUser.findOne({ username: username }, function (err, doc) {
+        if (doc) {
+            req.flash('error', 'Account already exists');
+            res.redirect('/profile/add');
+        } else {
+            dbUser.findOne({ email: email }, function (err, doc1) {
+                if (doc1) {
+                    req.flash('error', 'Email already exists');
+                    res.redirect('/profile/add');
+                } else {
+                    var newuser = new dbUser();
+                    newuser.username = username;
+                    newuser.password = newuser.createPass(password);
+                    newuser.firstname = firstname;
+                    newuser.lastname = lastname;
+                    newuser.email = email;
+                    newuser.phone = phone;
+                    newuser.country = country;
+                    newuser.active = active;
+                    if (avatar != undefined) {
+                        newuser.avatar = req.file.filename;
+                    }
+                    newuser.description = description;
+                    newuser.save((err) => {
+                        if (!err) {
+                            res.redirect('/profile');
+                        }
+                    });
+                }
+            })
         }
-        if(firstname != ''){
-            doc.firstname = firstname;
-        }
-        if(lastname != ''){
-            doc.lastname = lastname;
-        }
-        if(phone != ''){
-            doc.phone = phone
-        }
-        if(country != ''){
-            doc.country = country;
-        }
-        doc.active = active;
-        if(avatar != undefined){
-            avatar = req.file.filename;
-            if(doc.avatar == 'default-avatar.png'){
-                doc.avatar = avatar;
-            }else{
-                fs.unlink('public/img/user/' + doc.avatar , function(){});
-                doc.avatar = avatar;
-            }
-        }else{
-            avatar = 'default-avatar.png';
-            doc.avatar = avatar;
-        }
-        doc.description = description;
-        doc.save();
     })
 }
--- routers/profile.js
+++ routers/profile.js
@@ -10,8 +10,18 @@ var islog = function (req, res, next) {
     }
 }
 
-router.get('/:username', islog, controller.getProfile);
+router.get('/', islog, controller.getAllProfile);
+
+router.get('/edit/:username', islog, controller.getProfile);
 
 router.post('/edit/:username', islog, upload.single('avatar'), controller.postProfile);
+
+router.get('/see/:username', islog, controller.seeOneProfile);
+
+router.get('/delete/:id', islog, controller.deleteProfile);
+
+router.get('/add', islog, controller.addProfile);
+
+router.post('/add', islog, upload.single('avatar'), controller.addNewProfile);
 
 module.exports = router;
